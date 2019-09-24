2ea3645fa189853a93b3a477403c91cd17f2bfd6
esraod <oktavesra@gmail.com>
2019-09-24 16:59:44 +0100

    added some basic UI and made the water level visible on the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,37 @@
-import React from 'react';
+
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import io from 'socket.io-client';
 
 function App() {
+	const[level,setLevel]=useState('0');
   const socket = io('http://192.168.1.222:3000');
 
   socket.on('RECEIVE_DATA', (data) => {
     console.log(data);
+	setLevel(data);
+	console.log("variable works " + level);
   });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="app">
+    <header>
+      <h1>waterlevel</h1>
+      <h4>Bamse's waterbowl</h4>
+    </header>
+    <div className="watercontent">
+      <h2>The water level is</h2>
+      <h3>{level}</h3>
+
+      <div className="bowl">
+          <div className="water">
+        
+          </div>
+      </div>
     </div>
+      
+  </div>
   );
 }
 
